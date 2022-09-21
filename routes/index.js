284a69d20ea5b705509304ef88736b9d29aef304
NassimLimones78 <nassimisais@hotmail.com>
2022-09-20 23:20:07 -0500

    vistas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { title: 'Express' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
+
 
 module.exports = router;
